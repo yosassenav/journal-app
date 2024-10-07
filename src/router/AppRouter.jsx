2774ae19643d49d4d0f4a1d56df5171aeb296302
yosassenav <mromerovanessa.soy@gmail.com>
2024-10-06 22:58:30 -0500

    adding routes to the project

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import { AuthRoutes } from '../auth/routes/AuthRoutes'
+import { JournalRoutes } from './../journal/routes/JournalRoutes'
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+        {/**Login y registro */}
+        <Route path="/auth/*" element={<AuthRoutes/>}/>
+        {/**Journal */}
+        <Route path="/*" element={<JournalRoutes/>}/>
+    </Routes>
+  )
+}
